91b31df0eae1f1de711029028ca781a6c3756821
Adam Stevenson <stevenson_adam@yahoo.com>
2021-10-20 20:41:39 -0600

    Update ProviderConfiguration for CqlLoggingEngabled

--- src/main/java/org/opencds/cqf/cds/providers/ProviderConfiguration.java
+++ src/main/java/org/opencds/cqf/cds/providers/ProviderConfiguration.java
@@ -5,18 +5,20 @@ import ca.uhn.fhir.rest.api.SearchStyleEnum;
 public class ProviderConfiguration {
 
     public static final ProviderConfiguration DEFAULT_PROVIDER_CONFIGURATION = 
-        new ProviderConfiguration(true, 64, SearchStyleEnum.GET, 8000);
+        new ProviderConfiguration(true, 64, SearchStyleEnum.GET, 8000, false);
 
     private int maxCodesPerQuery;
     private SearchStyleEnum searchStyle;
     private boolean expandValueSets;
     private int maxUriLength;
+    private boolean cqlLoggingEnabled;
 
-    public ProviderConfiguration(boolean expandValueSets, int maxCodesPerQuery, SearchStyleEnum searchStyle, int maxUriLength) {
+    public ProviderConfiguration(boolean expandValueSets, int maxCodesPerQuery, SearchStyleEnum searchStyle, int maxUriLength, boolean cqlLoggingEnabled) {
         this.maxCodesPerQuery = maxCodesPerQuery;
         this.searchStyle = searchStyle;
         this.expandValueSets = expandValueSets;
         this.maxUriLength = maxUriLength;
+        this.cqlLoggingEnabled = cqlLoggingEnabled;
     }
 
     public int getMaxCodesPerQuery() {
@@ -34,4 +36,6 @@ public class ProviderConfiguration {
     public int getMaxUriLength() {
         return this.maxUriLength;
     }
+
+    public boolean getCqlLoggingEnabled() { return this.cqlLoggingEnabled; }
 }
